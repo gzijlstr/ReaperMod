b8635537f7dc4dab1ae542d8182bcc1f54bc3804
gzijlstr <gzijlstra8@gmail.com>
2023-12-19 18:04:25 +0100

    Add project files.

---
+++ Reaper_Plugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using CyclopsBeaconDeployer.Items;
+using HarmonyLib;
+using Nautilus.Handlers;
+using System.Collections;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace ReaperMod
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("com.snmodding.nautilus")]
+    public class Reaper_Plugin : BaseUnityPlugin
+    {
+        public new static ManualLogSource Logger { get; private set; }
+
+        private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+
+        private void Awake()
+        {
+            // plugin startup logic
+            Logger = base.Logger;
+
+            // register harmony patches, if there are any
+            Harmony.CreateAndPatchAll(Assembly, $"{PluginInfo.PLUGIN_GUID}");
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
