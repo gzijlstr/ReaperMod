a4b359ef668522083797789e4ac27f86f58df9cf
gzijlstr <gzijlstra8@gmail.com>
2023-12-21 16:56:20 +0100

    updated code plugin

---
+++ ReaperModOptions.cs
@@ -0,0 +1,15 @@
+﻿using Nautilus.Handlers;
+using Nautilus.Options;
+
+namespace ReaperMod
+{
+    public class ReaperModOptions : ModOptions
+    {
+        public ReaperModOptions() : base("Reaper Customizations")
+        {
+            AddItem(ModSliderOption.Create("SliderId", "Reaper Size", 0, 50, 15, 25));
+
+            OptionsPanelHandler.RegisterModOptions(this);
+        }
+    }
+}
