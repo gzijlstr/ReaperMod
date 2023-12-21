20f2c58c3a02dc35266e4d3c71beda8da11bb3cf
gzijlstr <gzijlstra8@gmail.com>
2023-12-21 20:48:47 +0100

    code fixes plugin and confid

--- ReaperModOptions.cs
+++ ReaperModOptions.cs
@@ -1,15 +1,28 @@
-﻿using Nautilus.Handlers;
-using Nautilus.Options;
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using Nautilus.Handlers;
+using Nautilius.Options;
+using Nautilius.Json;
+using Nautilus.Options.Attributes;
+using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
 
-namespace ReaperMod
+namespace Rilay.ReaperMod
 {
+
+
+
+
+
+    [Menu("Knife Damage Mod")]
     public class ReaperModOptions : ModOptions
     {
-        public ReaperModOptions() : base("Reaper Customizations")
-        {
-            AddItem(ModSliderOption.Create("SliderId", "Reaper Size", 0, 50, 15, 25));
 
-            OptionsPanelHandler.RegisterModOptions(this);
-        }
+        [Slider("Reaper Size", 1.0f, 100.0f, DefaultValue = 1.0f, Format = "{0:F2}")]
+
     }
 }
---
+++ ReaperModPlugin.cs
@@ -0,0 +1,46 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using Nautilus.Handlers;
+using Nautilius.Options;
+using Nautilius.Json;
+using Nautilus.Options.Attributes;
+using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+
+
+namespace Rilay.ReaperMod
+{
+
+
+    [BepInPlugin(MyGuid, PluginName, VersionString)]
+    [BepInDependency("com.snmodding.nautilus")] // marks Nautilus as a dependency for this mod
+
+
+    public class ReaperModPlugin : BaseUnityPlugin
+    {
+        private const string MyGuid = "com.Rilay.ReaperMod";
+        private const string PluginName = "Reaper Mod";
+        private const string VersionString = "1.0.0";
+
+        private static readonly Harmony Harmony = new.Harmony(MyGuid);
+
+
+        public static ManualLogSource Log;
+
+        // declaring mod options from ReaperModOptions.cs
+        public static ReaperModOptions ModOptions;
+
+        private void Awake()
+        {
+            ModOptions = OptionsPanelHandler.RegisterModOptions<ModOptions>();
+
+            Harmony.PatchAll();
+            Logger.LogInfo(PluginName + " " + VersionString + " " + "loaded.");
+            Log = Logger;
+        }
+    }
+}
